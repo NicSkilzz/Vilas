0b0abc69651b19d8bb0455d0a19a21953a180725
nklin <nklin02@gmail.com>
2025-12-04 16:02:22 +0100

    InteractionManager

---
+++ interaction/interaction_area.gd
@@ -0,0 +1,18 @@
+extends Area2D
+class_name InteractionArea
+
+
+@export var action_name:String = "interact"
+
+var interact: Callable = func():
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		InteractionManager.register_area(self)
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		InteractionManager.unregister_area(self)
--- scene/skill_scene.gd
+++ scene/skill_scene.gd
@@ -1,5 +1,17 @@
 extends Area2D
 
+@onready var interaction_area:InteractionArea = $InteractionArea
+
+
+func _ready() -> void:
+	interaction_area.interact = Callable(self, "on_interact")
+
+func on_interact():
+	var player = get_tree().get_first_node_in_group("Player")
+	player.can_use_ability_dash = true
+	$AnimatedSprite2D.play("open")
+
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		body.can_use_ability_dash = true
